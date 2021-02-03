30d6a8c872a23b10fc4a978ae31e315ba6dd0a52
Olivier Francois <olivier.francois.ext@orange.com>
2021-02-03 16:56:16 +0100

    Initialize repo

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,64 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Mon PortFolio Développeur Web FullStack', // e.g: 'Name | Developer'
+  lang: 'fr', // e.g: en, es, fr, jp
+  description: 'Bienvenu sur mon portfolio développeur web FullStack', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Salut, mon nom est',
+  name: 'Olivier',
+  subtitle: 'Je suis Développeur Web FullStack',
+  cta: 'En savoir plus',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.png',
+  paragraphOne: 'Développeur depuis bientôt 10 ans, j\'ai acquis une expertise dans le développement web FullStack. Actuellement en mission au sein d\'un grand groupe sur Sophia Antipolis, je perfectionne mes compétences et en découvrent de nouvelles chaque jour.',
+  paragraphTwo: 'HTML, CSS, Javascript, PHP, Java. Voilà ce que j\'adore faire.',
+  paragraphThree: 'Développeur la semaine, guitariste le week-end. La musique est ma deuxième passion et je joue actuellement dans plusieurs groupes.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'projet1.PNG',
+    title: 'Vignette Aide & Contact',
+    info: 'Développement de la vignette Aide et Contact de l\'Assistance Orange',
+    info2: 'Typescript / Grunt / Sass',
+    url: 'https://assistance.orange.fr/assistance-commerciale/l-identification/gerer-vos-mots-de-passe/compte-orange-mot-de-passe-oublie-ou-incorrect_216803-759648',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'projet2.png',
+    title: 'API FullContact',
+    info: 'Développement de l\'API FullContact pour Orange. Cet API est utilisée pour renvoyer les moyens de contacts à afficher dans un contexte particulier.',
+    info2: 'PHP 7, Slim 3, Docker, PhpUnit, Sonar',
+    url: 'https://assistance.orange.fr/assistance-commerciale/l-identification/gerer-vos-mots-de-passe/compte-orange-mot-de-passe-oublie-ou-incorrect_216803-759648',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'projet3.png',
+    title: 'Travel By AirFrance',
+    info: 'Développement des pages guide de voyage Air France',
+    info2: 'HTML, CSS, Java',
+    url: 'https://assistance.orange.fr/assistance-commerciale/l-identification/gerer-vos-mots-de-passe/compte-orange-mot-de-passe-oublie-ou-incorrect_216803-759648',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Vous voulez travaillez avec moi? Super !',
+  btn: 'Parlons en !',
+  email: 'oliv.francois@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +67,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: '',
-    },
+      url: 'https://linkedin.com/in/olivier-françois-61792b40/',
+    }
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
